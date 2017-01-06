28891d314251161b1b2b25da782c6bec6056628c
YushkinaS <YushkinaS@users.noreply.github.com>
2017-01-06 22:45:57 +0300

    Create CreateDataProviderBehavior.php

---
+++ CreateDataProviderBehavior.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\components;
+
+use yii\base\Behavior;
+use yii\data\ActiveDataProvider;
+
+class CreateDataProviderBehavior extends Behavior
+{
+    /**
+     * Create ActiveDataProvider
+     * @param \yii\db\ActiveQuery $query
+	 * @param array $pagination
+     * @return \yii\data\ActiveDataProvider
+     */
+    public function createDataProvider($query,$pagination=false)
+    {
+		$dataProvider = new ActiveDataProvider([
+            'query' => $query,
+			'pagination' => $pagination,
+        ]);
+        return $dataProvider ;
+    }
+
+}
